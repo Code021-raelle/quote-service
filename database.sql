1b59bf2282ebcbc891a25da3afa51376a42efc9e
Code021-raelle <gabrielakinshola3@gmail.com>
2024-05-27 13:31:04 +0100

    cita

--- database.sql
+++ database.sql
@@ -1,3 +1,7 @@
+-- prepares a MySQL server for the project
+
+mysql -u root -p;
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
